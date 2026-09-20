033735afbb87debc0db06fa584b6712458778059
Fixer <283546814+terminalfix@users.noreply.github.com>
2026-09-20 10:49:09 -0300

    Update configurador-inicial.bat

--- configurador-inicial.bat
+++ configurador-inicial.bat
@@ -1,4 +1,3 @@
-```bat
 @echo off
 color 0A
 title Configurador de Equipos Escolares - Region Educativa 7
@@ -29,18 +28,7 @@ title Configurador de Equipos Escolares - Region Educativa 7
 
 net session >nul 2>&1
 
-if %errorlevel% neq 0 (
-    echo.
-    echo ============================================
-    echo       ERROR: SE REQUIEREN PRIVILEGIOS
-    echo             DE ADMINISTRADOR
-    echo ============================================
-    echo.
-    echo Ejecute este archivo como Administrador.
-    echo.
-    pause
-    exit /b
-)
+if errorlevel 1 goto ERROR_ADMIN
 
 
 :: =====================================================
@@ -66,37 +54,44 @@ echo 5 - Educacion Especial
 echo.
 
 set "PREFIJO="
+set "TIPO="
 set /p TIPO=Seleccione opcion: 
 
-if "%TIPO%"=="1" (
-    set "PREFIJO=J"
-    goto NUMERO_INSTITUCION
-)
-
-if "%TIPO%"=="2" (
-    set "PREFIJO=EP"
-    goto NUMERO_INSTITUCION
-)
-
-if "%TIPO%"=="3" (
-    set "PREFIJO=EES"
-    goto NUMERO_INSTITUCION
-)
-
-if "%TIPO%"=="4" (
-    set "PREFIJO=EEST"
-    goto NUMERO_INSTITUCION
-)
-
-if "%TIPO%"=="5" (
-    set "PREFIJO=EEE"
-    goto NUMERO_INSTITUCION
-)
+if "%TIPO%"=="1" goto TIPO_JARDIN
+if "%TIPO%"=="2" goto TIPO_PRIMARIA
+if "%TIPO%"=="3" goto TIPO_SECUNDARIA
+if "%TIPO%"=="4" goto TIPO_TECNICA
+if "%TIPO%"=="5" goto TIPO_ESPECIAL
 
 echo.
 echo Opcion invalida.
 pause
 goto TIPO_INSTITUCION
+
+
+:TIPO_JARDIN
+set "PREFIJO=J"
+goto NUMERO_INSTITUCION
+
+
+:TIPO_PRIMARIA
+set "PREFIJO=EP"
+goto NUMERO_INSTITUCION
+
+
+:TIPO_SECUNDARIA
+set "PREFIJO=EES"
+goto NUMERO_INSTITUCION
+
+
+:TIPO_TECNICA
+set "PREFIJO=EEST"
+goto NUMERO_INSTITUCION
+
+
+:TIPO_ESPECIAL
+set "PREFIJO=EEE"
+goto NUMERO_INSTITUCION
 
 
 :: =====================================================
@@ -141,27 +136,32 @@ echo 3 - Hurlingham
 echo.
 
 set "DISTRITO="
+set "DIST="
 set /p DIST=Seleccione opcion: 
 
-if "%DIST%"=="1" (
-    set "DISTRITO=SM"
-    goto TIPO_EQUIPO
-)
-
-if "%DIST%"=="2" (
-    set "DISTRITO=TDF"
-    goto TIPO_EQUIPO
-)
-
-if "%DIST%"=="3" (
-    set "DISTRITO=H"
-    goto TIPO_EQUIPO
-)
+if "%DIST%"=="1" goto DISTRITO_SM
+if "%DIST%"=="2" goto DISTRITO_TDF
+if "%DIST%"=="3" goto DISTRITO_H
 
 echo.
 echo Opcion invalida.
 pause
 goto DISTRITO
+
+
+:DISTRITO_SM
+set "DISTRITO=SM"
+goto TIPO_EQUIPO
+
+
+:DISTRITO_TDF
+set "DISTRITO=TDF"
+goto TIPO_EQUIPO
+
+
+:DISTRITO_H
+set "DISTRITO=H"
+goto TIPO_EQUIPO
 
 
 :: =====================================================
@@ -177,67 +177,84 @@ echo             TIPO DE EQUIPO
 echo ============================================
 echo.
 
+if "%TIPO%"=="1" goto EQUIPO_JARDIN
+if "%TIPO%"=="2" goto EQUIPO_PRIMARIA
+if "%TIPO%"=="3" goto EQUIPO_SECUNDARIA
+if "%TIPO%"=="4" goto EQUIPO_SECUNDARIA
+if "%TIPO%"=="5" goto EQUIPO_SECUNDARIA
+
+goto TIPO_INSTITUCION
+
 
 :: =====================================================
 :: JARDIN
 :: =====================================================
 
-if "%TIPO%"=="1" (
-    echo 1 - Aprender Conectados
-    echo 2 - Prini
-    echo.
+:EQUIPO_JARDIN
 
-    set "SECTOR="
-    set /p AREA=Seleccione opcion: 
+echo 1 - Aprender Conectados
+echo 2 - Prini
+echo.
 
-    if "%AREA%"=="1" set "SECTOR=AC"
-    if "%AREA%"=="2" set "SECTOR=PRI"
+set "SECTOR="
+set "AREA="
+set /p AREA=Seleccione opcion: 
 
-    if not defined SECTOR (
-        echo.
-        echo Opcion invalida.
-        pause
-        goto TIPO_EQUIPO
-    )
+if "%AREA%"=="1" goto JARDIN_AC
+if "%AREA%"=="2" goto JARDIN_PRI
 
-    goto NUMERO_EQUIPO
-)
+echo.
+echo Opcion invalida.
+pause
+goto EQUIPO_JARDIN
+
+
+:JARDIN_AC
+set "SECTOR=AC"
+goto NUMERO_EQUIPO
+
+
+:JARDIN_PRI
+set "SECTOR=PRI"
+goto NUMERO_EQUIPO
 
 
 :: =====================================================
 :: PRIMARIA
 :: =====================================================
 
-if "%TIPO%"=="2" (
-    echo 1 - PAD
-    echo 2 - ADM
-    echo.
+:EQUIPO_PRIMARIA
 
-    set "SECTOR="
-    set /p AREA=Seleccione opcion: 
+echo 1 - PAD
+echo 2 - ADM
+echo.
 
-    if "%AREA%"=="1" set "SECTOR=PAD"
-    if "%AREA%"=="2" set "SECTOR=ADM"
+set "SECTOR="
+set "AREA="
+set /p AREA=Seleccione opcion: 
 
-    if not defined SECTOR (
-        echo.
-        echo Opcion invalida.
-        pause
-        goto TIPO_EQUIPO
-    )
+if "%AREA%"=="1" goto PRIMARIA_PAD
+if "%AREA%"=="2" goto PRIMARIA_ADM
 
-    goto NUMERO_EQUIPO
-)
+echo.
+echo Opcion invalida.
+pause
+goto EQUIPO_PRIMARIA
+
+
+:PRIMARIA_PAD
+set "SECTOR=PAD"
+goto NUMERO_EQUIPO
+
+
+:PRIMARIA_ADM
+set "SECTOR=ADM"
+goto NUMERO_EQUIPO
 
 
 :: =====================================================
 :: SECUNDARIA / TECNICA / EDUCACION ESPECIAL
 :: =====================================================
-
-if "%TIPO%"=="3" goto EQUIPO_SECUNDARIA
-if "%TIPO%"=="4" goto EQUIPO_SECUNDARIA
-if "%TIPO%"=="5" goto EQUIPO_SECUNDARIA
-
 
 :EQUIPO_SECUNDARIA
 
@@ -248,19 +265,38 @@ echo 4 - Aprender Conectados
 echo.
 
 set "SECTOR="
+set "AREA="
 set /p AREA=Seleccione opcion: 
 
-if "%AREA%"=="1" set "SECTOR=CI"
-if "%AREA%"=="2" set "SECTOR=CIB"
-if "%AREA%"=="3" set "SECTOR=JM"
-if "%AREA%"=="4" set "SECTOR=AC"
+if "%AREA%"=="1" goto SECUNDARIA_CI
+if "%AREA%"=="2" goto SECUNDARIA_CIB
+if "%AREA%"=="3" goto SECUNDARIA_JM
+if "%AREA%"=="4" goto SECUNDARIA_AC
 
-if not defined SECTOR (
-    echo.
-    echo Opcion invalida.
-    pause
-    goto TIPO_EQUIPO
-)
+echo.
+echo Opcion invalida.
+pause
+goto EQUIPO_SECUNDARIA
+
+
+:SECUNDARIA_CI
+set "SECTOR=CI"
+goto NUMERO_EQUIPO
+
+
+:SECUNDARIA_CIB
+set "SECTOR=CIB"
+goto NUMERO_EQUIPO
+
+
+:SECUNDARIA_JM
+set "SECTOR=JM"
+goto NUMERO_EQUIPO
+
+
+:SECUNDARIA_AC
+set "SECTOR=AC"
+goto NUMERO_EQUIPO
 
 
 :: =====================================================
@@ -330,40 +366,102 @@ echo.
 
 wmic computersystem where name="%COMPUTERNAME%" call rename name="%NUEVO_NOMBRE%" >nul
 
-if %errorlevel% neq 0 (
-    echo ERROR: No se pudo cambiar el nombre del equipo.
-    echo.
-    pause
-    exit /b
-)
+if errorlevel 1 goto ERROR_NOMBRE
 
 echo Nombre cambiado correctamente.
 
 
 :: =====================================================
-:: CONFIGURAR MEMORIA VIRTUAL
+:: CONFIGURAR MEMORIA VIRTUAL SEGUN RAM
 :: =====================================================
 
 echo.
+echo Detectando memoria RAM...
+echo.
+
+set "RAM_BYTES="
+set "RAM_GB="
+set "PAGE_MIN="
+set "PAGE_MAX="
+
+for /f "skip=1" %%A in ('wmic computersystem get TotalPhysicalMemory') do (
+    if not defined RAM_BYTES set "RAM_BYTES=%%A"
+)
+
+if not defined RAM_BYTES goto ERROR_RAM
+
+set /a RAM_GB=RAM_BYTES/1024/1024/1024
+
+echo Memoria RAM detectada : %RAM_GB% GB
+echo.
+
+
+:: =====================================================
+:: ASIGNAR MEMORIA VIRTUAL
+:: =====================================================
+
+if "%RAM_GB%"=="1" goto RAM_1GB
+if "%RAM_GB%"=="2" goto RAM_2GB
+if "%RAM_GB%"=="4" goto RAM_4GB
+if "%RAM_GB%"=="8" goto RAM_8GB
+
+goto ERROR_RAM_NO_COMPATIBLE
+
+
+:RAM_1GB
+set "PAGE_MIN=1024"
+set "PAGE_MAX=2048"
+goto CONFIGURAR_PAGEFILE
+
+
+:RAM_2GB
+set "PAGE_MIN=2048"
+set "PAGE_MAX=4096"
+goto CONFIGURAR_PAGEFILE
+
+
+:RAM_4GB
+set "PAGE_MIN=4096"
+set "PAGE_MAX=8192"
+goto CONFIGURAR_PAGEFILE
+
+
+:RAM_8GB
+set "PAGE_MIN=8192"
+set "PAGE_MAX=16384"
+goto CONFIGURAR_PAGEFILE
+
+
+:: =====================================================
+:: APLICAR MEMORIA VIRTUAL
+:: =====================================================
+
+:CONFIGURAR_PAGEFILE
+
+echo.
+echo Configuracion de memoria virtual:
+echo.
+echo Minimo : %PAGE_MIN% MB
+echo Maximo : %PAGE_MAX% MB
+echo.
+
 echo Configurando memoria virtual...
 echo.
 
 wmic computersystem where name="%COMPUTERNAME%" set AutomaticManagedPagefile=False >nul
 
+if errorlevel 1 goto ERROR_PAGEFILE
+
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" ^
-/v PagingFiles /t REG_MULTI_SZ /d "C:\pagefile.sys 8192 16384" /f >nul
+/v PagingFiles /t REG_MULTI_SZ /d "C:\pagefile.sys %PAGE_MIN% %PAGE_MAX%" /f >nul
 
-if %errorlevel% neq 0 (
-    echo ERROR: No se pudo configurar la memoria virtual.
-    echo.
-    pause
-    exit /b
-)
+if errorlevel 1 goto ERROR_PAGEFILE
 
-echo Memoria virtual configurada:
 echo.
-echo Minimo : 8192 MB
-echo Maximo : 16384 MB
+echo Memoria virtual configurada correctamente.
+echo.
+echo Minimo : %PAGE_MIN% MB
+echo Maximo : %PAGE_MAX% MB
 
 
 :: =====================================================
@@ -378,6 +476,12 @@ echo.
 echo Nombre del equipo:
 echo %NUEVO_NOMBRE%
 echo.
+echo Memoria RAM:
+echo %RAM_GB% GB
+echo.
+echo Memoria virtual:
+echo %PAGE_MIN% MB - %PAGE_MAX% MB
+echo.
 echo El equipo se reiniciara en 15 segundos.
 echo.
 echo Guarde su trabajo antes de continuar.
@@ -387,4 +491,81 @@ echo.
 timeout /t 15 /nobreak >nul
 
 shutdown /r /t 0
-```
+
+exit /b
+
+
+:: =====================================================
+:: ERRORES
+:: =====================================================
+
+:ERROR_ADMIN
+
+echo.
+echo ============================================
+echo       ERROR: SE REQUIEREN PRIVILEGIOS
+echo             DE ADMINISTRADOR
+echo ============================================
+echo.
+echo Ejecute este archivo como Administrador.
+echo.
+pause
+exit /b
+
+
+:ERROR_NOMBRE
+
+echo.
+echo ============================================
+echo       ERROR AL CAMBIAR EL NOMBRE
+echo ============================================
+echo.
+echo No se pudo cambiar el nombre del equipo.
+echo.
+pause
+exit /b
+
+
+:ERROR_RAM
+
+echo.
+echo ============================================
+echo       ERROR AL DETECTAR LA MEMORIA RAM
+echo ============================================
+echo.
+echo No se pudo determinar la memoria RAM instalada.
+echo La configuracion de memoria virtual no se modificara.
+echo.
+pause
+exit /b
+
+
+:ERROR_RAM_NO_COMPATIBLE
+
+echo.
+echo ============================================
+echo       MEMORIA RAM NO CONTEMPLADA
+echo ============================================
+echo.
+echo RAM detectada: %RAM_GB% GB
+echo.
+echo Este configurador contempla equipos de:
+echo 1 GB, 2 GB, 4 GB y 8 GB de RAM.
+echo.
+echo La configuracion de memoria virtual no se modificara.
+echo.
+pause
+exit /b
+
+
+:ERROR_PAGEFILE
+
+echo.
+echo ============================================
+echo       ERROR AL CONFIGURAR MEMORIA VIRTUAL
+echo ============================================
+echo.
+echo No se pudo configurar el archivo de paginacion.
+echo.
+pause
+exit /b
